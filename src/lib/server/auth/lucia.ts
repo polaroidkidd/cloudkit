f9e3cb78e477a6b17cff5f4aafb910b45d66e7b0
Daniel Einars <contact@dle.dev>
2023-10-29 16:22:27 +0100

    use eventualAuth to include correct dependencies at build time
    Signed-off-by: Daniel Einars <contact@dle.dev>

--- src/lib/server/auth/lucia.ts
+++ src/lib/server/auth/lucia.ts
@@ -7,56 +7,118 @@ import { sveltekit } from 'lucia/middleware';
 
 import { dev } from '$app/environment';
 import { IS_CI, REDIS_TOKEN, REDIS_URL } from '$env/static/private';
-import { upstash as prodRedis, redis as devRedis } from '@lucia-auth/adapter-session-redis';
-// PROD
-import { Redis as createProdRedisClient } from '@upstash/redis/cloudflare';
-// DEV or CI
-import { createClient as createLocalRedisClient } from 'redis';
+import { redis as devRedis, upstash as prodRedis } from '@lucia-auth/adapter-session-redis';
+import type { Auth as LuciaAuth, Configuration } from 'lucia';
 
-function createSessionConfiguration() {
-	if (dev || IS_CI === 'true') {
-		console.info('Using local redis client');
-		const redisClient = createLocalRedisClient({
-			url: REDIS_URL
-		});
-
-		redisClient.connect();
-		redisClient.on('error', (err) => console.log('Redis Client Error', err));
-		redisClient.on('ready', () => console.log('Redis Client Ready'));
-		return devRedis(redisClient);
-	}
-	return prodRedis(
-		new createProdRedisClient({
-			url: REDIS_URL,
-			token: REDIS_TOKEN
-		})
-	);
-}
-
-export const auth = lucia({
-	adapter: {
-		user: prisma(db),
-		session: createSessionConfiguration()
-	},
-
-	env: dev ? 'DEV' : 'PROD',
-	middleware: sveltekit(),
-
-	getUserAttributes: (data) => {
-		return {
-			firstName: data.firstName,
-			lastName: data.lastName,
-			email: data.email,
-			bio: data.bio,
-			avatarId: data.avatarId,
-			createdAt: data.createdAt,
-			updatedAt: data.updatedAt,
-			verified: data.verified
+type ProdAuth = LuciaAuth<
+	Configuration & {
+		adapter: {
+			user: ReturnType<typeof prisma>;
+			session: ReturnType<typeof prodRedis>;
 		};
+		env: 'DEV' | 'PROD';
+		middleware: ReturnType<typeof sveltekit>;
+		getUserAttributes: (data: unknown) => {
+			firstName: string;
+			lastName: string;
+			email: string;
+			bio: string;
+			avatarId: string;
+			createdAt: Date;
+			updatedAt: Date;
+			verified: boolean;
+		};
+	}
+>;
+
+type DevAuth = LuciaAuth<
+	Configuration & {
+		adapter: {
+			user: ReturnType<typeof prisma>;
+			session: ReturnType<typeof devRedis>;
+		};
+		env: 'DEV' | 'PROD';
+		middleware: ReturnType<typeof sveltekit>;
+		getUserAttributes: (data: unknown) => {
+			firstName: string;
+			lastName: string;
+			email: string;
+			bio: string;
+			avatarId: string;
+			createdAt: Date;
+			updatedAt: Date;
+			verified: boolean;
+		};
+	}
+>;
+export const eventualAuth: Promise<ProdAuth | DevAuth> = new Promise((resolve) => {
+	if (dev || IS_CI === 'true') {
+		import('redis').then(({ createClient }) => {
+			const redisClient = createClient({
+				url: REDIS_URL
+			});
+
+			redisClient.connect();
+			redisClient.on('error', (err) => console.log('Redis Client Error', err));
+			redisClient.on('ready', () => console.log('Redis Client Ready'));
+
+			const auth = lucia({
+				adapter: {
+					user: prisma(db),
+					session: devRedis(redisClient)
+				},
+				env: dev ? 'DEV' : 'PROD',
+				middleware: sveltekit(),
+				getUserAttributes: (data) => {
+					return {
+						firstName: data.firstName,
+						lastName: data.lastName,
+						email: data.email,
+						bio: data.bio,
+						avatarId: data.avatarId,
+						createdAt: data.createdAt,
+						updatedAt: data.updatedAt,
+						verified: data.verified
+					};
+				}
+			});
+			resolve(auth as DevAuth);
+		});
+	} else {
+		import('@upstash/redis/cloudflare').then((Redis) => {
+			const auth = lucia({
+				adapter: {
+					user: prisma(db),
+					session: prodRedis(
+						new Redis.Redis({
+							url: REDIS_URL,
+							token: REDIS_TOKEN
+						})
+					)
+				},
+				env: dev ? 'DEV' : 'PROD',
+				middleware: sveltekit(),
+				getUserAttributes: (data) => {
+					return {
+						firstName: data.firstName,
+						lastName: data.lastName,
+						email: data.email,
+						bio: data.bio,
+						avatarId: data.avatarId,
+						createdAt: data.createdAt,
+						updatedAt: data.updatedAt,
+						verified: data.verified
+					};
+				}
+			});
+			resolve(auth as ProdAuth);
+		});
 	}
 });
 
-export type Auth = typeof auth;
+export const auth = await eventualAuth;
+
+export type AuthType = Awaited<typeof auth>;
 
 /**
  *
