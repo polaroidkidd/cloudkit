34cfbb20175114cbd0994a2db15703c4c375da2d
Daniel Einars <contact@dle.dev>
2024-10-18 22:20:19 +0200

    delete redundant file

--- apps/swagger-ui/src/routes/swagger.json/+server.ts
+++
@@ -1,8 +0,0 @@
-export const GET = async () => {
-	const spec = await import('@cloudkit/service-contract');
-	return new Response(JSON.stringify(spec), {
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	});
-};
