163e31fff064a6ef6f2d6780186121bc166e46bb
Daniel Einars <polaroidkidd@users.noreply.github.com>
2024-10-31 14:07:36 +0100

    update merge to master action by deleting concurrency (#29)

--- apps/web/src/lib/api/base-api-service.ts
+++ apps/web/src/lib/api/base-api-service.ts
@@ -1,4 +1,4 @@
-import type { AxiosError, AxiosInstance, AxiosRequestConfig } from 'axios';
+import type { AxiosInstance, AxiosRequestConfig } from 'axios';
 import axios from 'axios';
 
 export abstract class ApiServiceBase {
@@ -31,27 +31,6 @@ export abstract class ApiServiceBase {
 			headers,
 			baseURL: context,
 			paramsSerializer: { indexes: null }
-		});
-		// /**
-		//  * Use this to log out all requests during a test run.
-		//  */
-		// if (process.env.NODE_ENV === 'test') {
-		//     this.http.interceptors.request.use(
-		//         (config) => {
-		//             console.log(`${config.method.toUpperCase()} ${config.baseURL}${config.url}`);
-		//
-		//             return config;
-		//         }
-		//     );
-		// }
-
-		this.http.interceptors.response.use(null, (error: AxiosError) => {
-			if (error.response && typeof error.response.data === 'string') {
-				// verifySessionTimeout(error.response);
-				// verifyMaintenanceMode(error.response as AxiosResponse<string>);
-			}
-
-			return Promise.reject(error);
 		});
 	}
 }
